45b203be78aa6dd2c586f1ec7517119109cf1701
Dallas Read <dallas@excitecreative.ca>
2015-01-21 02:33:41 -0400

    Little bit of cleanup. Centralizing apps.

--- app/routes/dashboard/index.js
+++ app/routes/dashboard/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	beforeModel: function() {
-		this.transitionTo( 'dashboard.people' );
+		this.transitionTo( 'dashboard.apps' );
 	}
 });
