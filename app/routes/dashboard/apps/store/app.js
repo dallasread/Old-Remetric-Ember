d0a283f5218c84a01ba2cae92ab3817724f22956
Dallas Read <dallas@excitecreative.ca>
2015-01-30 16:03:55 -0400

    in good shape.

--- app/routes/dashboard/apps/store/app.js
+++ app/routes/dashboard/apps/store/app.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function(params) {
-		this.set( 'session.currentApp', this.store.find('app', 'store') );
 		return this.store.find('app', params.app_id);
   }
 });
